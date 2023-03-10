96f7ed575fff5caf77a69925ac4679d8239794f1
Brandon(Suin) Park <Suin.Park@tlgcohort.com>
2023-03-10 15:52:18 -0800

    1st Iteration on User

--- src/com/won/model/User.java
+++ src/com/won/model/User.java
@@ -1,5 +1,45 @@
 package com.won.model;
 
-class User {
+import java.util.Scanner;
 
+class User {
+    private double budget;
+    private double hours;
+    private String location;
+
+    public User(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How much money do you plan to spend?");
+        budget = scanner.nextDouble();
+        System.out.println("How many hours do you plan to spend?");
+        hours = scanner.nextDouble();
+        System.out.println("Select a city from a following options:?");
+        System.out.println("1. Seattle");
+        System.out.println("2. Denver");
+        String city = scanner.next().toLowerCase();
+
+        switch (city) {
+            case "1":
+                city = "Seattle";
+                break;
+            case "2":
+                city = "Denver";
+                break;
+            default:
+                System.out.println("No other choices of city as of now.");
+                break;
+        }
+    }
+
+    public double getMoney() {
+        return budget;
+    }
+
+    public double getHours() {
+        return hours;
+    }
+
+    public String getLocation() {
+        return location;
+    }
 }
